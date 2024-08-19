345833d1a3b3234f9492c22c2cca946e367eb504
lizandra.malta <lizlrmalta@gmail.com>
2024-08-19 20:47:34 -0300

    atividade 1

---
+++ variaveisOperadores.js
@@ -0,0 +1,17 @@
+const numA = 8;
+const numB = 7;
+let resultado;
+
+resultado = numA + numB;
+console.log(`Resultado da soma entre ${numA} e ${numB} é ${resultado}\n`);
+
+resultado = numA - numB;
+console.log(`Resultado da subtração entre ${numA} e ${numB} é ${resultado}\n`);
+
+resultado = numA * numB;
+console.log(
+  `Resultado da multiplicação entre ${numA} e ${numB} é ${resultado}\n`
+);
+
+resultado = numA / numB;
+console.log(`Resultado da divisão entre ${numA} e ${numB} é ${resultado}\n`);
